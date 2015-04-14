057ddf6bc0a47f2d194f7955d5443c7bfe5a0ccc
Alex Viscreanu <alvis1@inf.upv.es>
2015-04-14 22:39:09 +0200

    Casi casi...

--- src/js/gallery.js
+++
@@ -1,8 +0,0 @@
-$(document).on('load', '#galleryWrapper', function(event) {
-	event.preventDefault();
-	imagelist = $('.stlimg');
-	srclist = [];
-	for (var i = 0; i < imagelist.length; i++) {
-		srclist[i]=$(imagelist[i]).attr('src');
-	};
-});
